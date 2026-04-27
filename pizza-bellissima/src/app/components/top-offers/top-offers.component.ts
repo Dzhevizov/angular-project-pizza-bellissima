d70df3dbdbf9c7ec44053ac585b918ba3ff09d72
Hristo Dzhevizov <dzhevizov@abv.bg>
2026-04-27 21:55:35 +0300

    change top-offers to reuse product-card component

--- pizza-bellissima/src/app/components/top-offers/top-offers.component.ts
+++ pizza-bellissima/src/app/components/top-offers/top-offers.component.ts
@@ -3,21 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Product } from '../../models/product.model';
 import { products } from '../../data/products';
-import { CartService } from '../../services/cart.service';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-top-offers',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ProductCardComponent],
   templateUrl: './top-offers.component.html',
   styleUrls: ['./top-offers.component.css'],
 })
 export class TopOffersComponent {
   topOffers: Product[] = products.slice(0, 4);
-
-  constructor(private cartService: CartService) {}
-
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
-  }
 }
